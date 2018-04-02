e0b3ee56fcb25be48d7b6c4ed35b846d5911a7f1
Vadim Gorbachev <bmsdave@gmail.com>
2018-04-02 18:08:51 +0300

    start to aggregate all to config.js

---
+++ config.js
@@ -0,0 +1,70 @@
+module.exports = {
+  siteTitle: "Vadim Gorbachev", // Site title.
+  shortSiteTitle: "",
+  siteTitleAlt: "", // Alternative site title for SEO.
+  siteLogo: "", // Logo used for SEO and manifest.
+  siteUrl: "", // Domain of your website without pathPrefix.
+  pathPrefix: "/",
+  siteDescription:
+    "", // Website description used for RSS feeds/meta description tag.
+  siteRss: "", // Path to the RSS file.
+  siteFBAppID: "", // FB Application ID for using app insights
+  googleAnalyticsID: "", // GA tracking ID.
+  userName: "", // Username to display in the author segment.
+  userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "", // User location to display in the author segment.
+  userAvatar: "", // User avatar to display in the author segment.
+  userDescription: "", // User description to display in the author segment.
+  // Links to social profiles/projects you want to display in the author segment/navigation bar.
+  userLinks: [
+    {
+      label: "",
+      href: "",
+      type: "gmail"
+    },
+    {
+      label: "",
+      href: "",
+      type: "twitter"
+    },
+    {
+      label: "",
+      href: "",
+      type: "github"
+    },
+    {
+      label: "",
+      href: "",
+      type: "instagram"
+    },
+    {
+      label: "",
+      href: "",
+      type: "linkedin"
+    }
+  ],
+  techInterestedIn: [
+    {
+      label: "",
+      href: ""
+    },
+    {
+      label: "",
+      href: ""
+    },
+    {
+      label: "",
+      href: ""
+    },
+    {
+      label: "",
+      href: ""
+    },
+    {
+      label: "",
+      href: ""
+    }
+  ],
+  themeColor: "#89bcfe", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#e0e0e0" // Used for setting manifest background color.
+};
