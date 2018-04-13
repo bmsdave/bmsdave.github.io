a3f932df072f68b5c7cde1b787674d78bd5fbf6a
Vadim Gorbachev <bmsdave@gmail.com>
2018-04-13 16:03:31 +0300

    Updates

--- sw.js
+++ sw.js
@@ -37,8 +37,8 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-1a5f9f87e29f1ef9cc78.js","f3a4250956c46825951361bd5ec98484"],["/app-7106ab56ad28d2c6c12c.js","21b9131fbf59f3d2e7de30a56b069a93"],["/app-c37dbc8e6ff372226478.js","a97d5be625164e1dd6bd6cbb00adef1a"],["/commons-74624da3bf8063cc10cb.js","44bdd775c5b9c81ec8c106e7d7460d44"],["/commons-c8eb99c75de2b8d777c4.js","bd7b1e5fb54bd7b5e9ed24306f82eb60"],["/index.html","399cb9fb992d5cee703e06c3f36a98de"],["/offline-plugin-app-shell-fallback/index.html","4f352c40942478b2846af9aa60495588"]];
-var cacheName = 'sw-precache-v3-gatsby-plugin-offline-5-' + (self.registration ? self.registration.scope : '');
+var precacheConfig = [["/app-0e9a5e2e2d292871372d.js","ca85d36ff66880a4ac53358e98a6898b"],["/app-1a5f9f87e29f1ef9cc78.js","f3a4250956c46825951361bd5ec98484"],["/app-7106ab56ad28d2c6c12c.js","21b9131fbf59f3d2e7de30a56b069a93"],["/app-c37dbc8e6ff372226478.js","a97d5be625164e1dd6bd6cbb00adef1a"],["/commons-25fce9ccd71047b5e4be.js","0595dae5173e5462667c8a2ab1657e00"],["/commons-74624da3bf8063cc10cb.js","44bdd775c5b9c81ec8c106e7d7460d44"],["/commons-c8eb99c75de2b8d777c4.js","bd7b1e5fb54bd7b5e9ed24306f82eb60"],["/index.html","105483eb556465024b3d3a1431ccb16c"],["/offline-plugin-app-shell-fallback/index.html","7bf0ba18cf595eb33ebb5115a8f29fe3"]];
+var cacheName = 'sw-precache-v3-gatsby-plugin-offline-6-' + (self.registration ? self.registration.scope : '');
 
 
 var ignoreUrlParametersMatching = [/^utm_/];
