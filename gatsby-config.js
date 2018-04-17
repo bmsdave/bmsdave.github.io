858cb0744db81c55c5cabe55ff1462fd2e1c54f2
Vadim Gorbachev <bmsdave@gmail.com>
2018-04-17 13:22:17 +0300

    update cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -119,7 +119,7 @@ module.exports = {
       options: {
         navigateFallback: null,
         navigateFallbackWhitelist: [],
-        cacheId: 'gatsby-plugin-offline-6',
+        cacheId: 'gatsby-plugin-offline-7',
       },
     },
     {
