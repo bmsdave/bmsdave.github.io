c7fa6102d3fa3542ae5255dd899564dbfd7c7b59
Vadim Gorbachev (bmsdave) <bmsdave@gmail.com>
2018-12-21 11:58:39 +0300

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-86431ad8ee934a80bac5.js","eb615ad813dcd441204cf8f6421eacd0"],["/app-f1c289bcf46ac0dd4559.js","249649295126341e4194c0685b14c95f"],["/commons-03d0f597f54f385a56ba.js","2c82580ef9a390d68464dfc6d292f722"],["/index.html","df4df7a6037e93cd723232a0a6214720"],["/offline-plugin-app-shell-fallback/index.html","6fc4026c3030ffa95a45b25bbbe07dd3"]];
+var precacheConfig = [["/app-86431ad8ee934a80bac5.js","eb615ad813dcd441204cf8f6421eacd0"],["/app-f1c289bcf46ac0dd4559.js","249649295126341e4194c0685b14c95f"],["/commons-03d0f597f54f385a56ba.js","2c82580ef9a390d68464dfc6d292f722"],["/index.html","467cf59a6fff04a8c9b80c3339d9d90c"],["/offline-plugin-app-shell-fallback/index.html","99c769f999d00fc0ad28bc45a59b65e9"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-9-' + (self.registration ? self.registration.scope : '');
 
 
