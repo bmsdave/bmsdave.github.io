8ea1c9607ca6ce1add1ee9f7cdc5e91fc64bcc3a
Vadim Gorbachev (bmsdave) <bmsdave@gmail.com>
2019-01-25 22:46:30 +0300

    Updates

--- path---blog-tags-dotjs-de7d11fb14e1500d0978.js
+++ path---blog-tags-dotjs-de7d11fb14e1500d0978.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xc4a77b0ef110],{409:function(a,t){a.exports={data:{allMarkdownRemark:null},pathContext:{tag:"dotjs"}}}});
+webpackJsonp([0xc4a77b0ef110],{411:function(a,t){a.exports={data:{allMarkdownRemark:null},pathContext:{tag:"dotjs"}}}});
 //# sourceMappingURL=path---blog-tags-dotjs-de7d11fb14e1500d0978.js.map
--- path---blog-tags-first-0ee93dc28471c8133b9a.js
+++ path---blog-tags-first-0ee93dc28471c8133b9a.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xa637a3f0a6a4],{410:function(t,a){t.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{fields:{slug:"/blog/first-post"},excerpt:"Hello world!",timeToRead:1,frontmatter:{title:"First Post",tags:["first"],date:"2018-04-07"}}}]}},pathContext:{tag:"first"}}}});
+webpackJsonp([0xa637a3f0a6a4],{412:function(t,a){t.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{fields:{slug:"/blog/first-post"},excerpt:"Hello world!",timeToRead:1,frontmatter:{title:"First Post",tags:["first"],date:"2018-04-07"}}}]}},pathContext:{tag:"first"}}}});
 //# sourceMappingURL=path---blog-tags-first-0ee93dc28471c8133b9a.js.map
