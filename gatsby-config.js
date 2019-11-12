278fa5184d2558485e9db62ee9cbcbebbf2f725b
Vadim <bmsdave@gmail.com>
2019-11-12 13:35:01 +0300

    add video from frontend conf

--- gatsby-config.js
+++ gatsby-config.js
@@ -117,7 +117,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-offline`,
       options: {
-        cacheId: 'gatsby-plugin-offline-16',
+        cacheId: 'gatsby-plugin-offline-17',
       },
     },
     {
