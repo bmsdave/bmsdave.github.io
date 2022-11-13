7cf47fce84dbaaa9401b2472a298f291cc150ec7
Vadim Gorbachev <vgorbachev@fb.com>
2022-11-13 20:40:05 +0000

    remove sw

--- src/html.jsx
+++ src/html.jsx
@@ -1,51 +1,57 @@
 /* eslint import/no-unresolved:"off" */
 /* eslint import/extensions:"off" */
 /* eslint global-require:"off" */
-import React from "react";
-import favicon from "./favicon.ico";
+import React from 'react'
+import favicon from './favicon.ico'
 
-let inlinedStyles = "";
-if (process.env.NODE_ENV === "production") {
-    try {
-        /* eslint import/no-webpack-loader-syntax: off */
-        inlinedStyles = require("!raw-loader!../public/styles.css");
-    } catch (e) {
-        /* eslint no-console: off */
-        console.log(e);
-    }
+let inlinedStyles = ''
+if (process.env.NODE_ENV === 'production') {
+  try {
+    /* eslint import/no-webpack-loader-syntax: off */
+    inlinedStyles = require('!raw-loader!../public/styles.css')
+  } catch (e) {
+    /* eslint no-console: off */
+    console.log(e)
+  }
+}
+
+if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+  window.navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(r => r.unregister())
+  })
 }
 
 export default class HTML extends React.Component {
-    render() {
-        let css;
-        if (process.env.NODE_ENV === "production") {
-            css = (
-                <style
-                    id="gatsby-inlined-css"
-                    dangerouslySetInnerHTML={{__html: inlinedStyles}}
-                />
-            );
-        }
-        return (
-            <html lang="en">
-            <head>
-                <meta charSet="utf-8"/>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
-                {this.props.headComponents}
-                <link rel="shortcut icon" href={favicon}/>
-                {css}
-            </head>
-            <body>
-            <div
-                id="___gatsby"
-                dangerouslySetInnerHTML={{__html: this.props.body}}
-            />
-            {this.props.postBodyComponents}
-            </body>
-            </html>
-        );
+  render() {
+    let css
+    if (process.env.NODE_ENV === 'production') {
+      css = (
+        <style
+          id="gatsby-inlined-css"
+          dangerouslySetInnerHTML={{ __html: inlinedStyles }}
+        />
+      )
     }
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          {this.props.headComponents}
+          <link rel="shortcut icon" href={favicon} />
+          {css}
+        </head>
+        <body>
+          <div
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    )
+  }
 }
